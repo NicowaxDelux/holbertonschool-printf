c8c653ace878564aacaf4f8258380cdb4076731e
smorkl <smsalazar0319@gmail.com>
2024-03-27 16:44:48 -0500

    modificar

--- codigo.c
+++ codigo.c
@@ -1,30 +1,41 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
 
-void _string(va_list navegar)
-{
-	char *str;
-
-	str = va_arg(navegar, char *);
-
-	while (*str != '\0')
-	{
-		putchar(*str);
-		str++;
-	}
-}
-
+/**
+ *
+ *
+ *
+ */
 
 int _printf(const char *format, ...)
 {
+	va_list args;
+	int chars_printed = 0;
+
+	va_start(args, format);
+
 	while (*format)
 	{
-		va_list navegar;
-
-		va_start(navegar, format);
-
-		switch
-
-
+		switch (*format)
+		{
+		case 's':
+			chars_printed += _string(args);
+			break;
+		}
 	}
+	va_end(args);
+
+	return (chars_printed);
+}
+
+int main(void)
+{
+    int chars_printed;
+
+	chars_printed = _printf("The answer is: %s\n", "42");
+
+	printf("\nNumber of characters printed: %d\n", chars_printed);
+
+    return 0;
 }

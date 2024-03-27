0f55999a1f35a89e68e05c7769f38cff859ec500
smorkl <smsalazar0319@gmail.com>
2024-03-27 14:22:51 -0500

    modificar

---
+++ printf_s.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * _string - print pf the string.
+ * @args: the string.
+ */
+
+void _string(va_list args)
+{
+	char *str;
+	int cant = 0;
+
+	str = va_arg(args, char *);
+
+	while (*str != '\0')
+	{
+		putchar(*str);
+		str++;
+		cant++;
+	}
+	return (cant);
+}

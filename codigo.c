29da96cfdde332f1029dd77fca1d7afc15a817a0
smorkl <smsalazar0319@gmail.com>
2024-03-27 08:17:20 -0500

    modificacion

--- codigo.c
+++ codigo.c
@@ -1,1 +1,17 @@
 #include <stdio.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...)
+{
+	while (*format)
+	{
+		va_list navegar;
+
+		va_start(navegar, format);
+
+		for (; *string != '\0'; string++)
+		{
+			putchar(*string);
+		}
+	}
+}

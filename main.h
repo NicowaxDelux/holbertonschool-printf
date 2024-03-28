809c0517460167d46a7e54a289d129596db05f7f
smorkl <smsalazar0319@gmail.com>
2024-03-27 19:48:43 -0500

    modificar

--- main.h
+++ main.h
@@ -1,1 +1,19 @@
-ss
+#ifndef	MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct {
+    const char *type;
+    void (*f)(va_list);
+} convert;
+
+void print_char(va_list args);
+int _putchar(char c);
+int _string(va_list args);
+int _printf(const char *format, ...);
+
+
+#endif
